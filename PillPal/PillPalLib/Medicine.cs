bb40eacc2cd57a1c326b094b61bb5df62dd9b56c
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2024-11-04 11:23:40 +0100

    Added Medicine and Reminder classes

---
+++ PillPal/PillPalLib/Medicine.cs
@@ -0,0 +1,28 @@
+﻿using PillPalLib.Interfaces;
+
+namespace PillPalLib
+{
+    public class Medicine : IIdentified
+    {
+        public Medicine(int id, string name, string description, int[] packageSizes, string manufacturer, string[] remedyFor, string[] activeIngredients, string[] sideEffects)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            PackageSizes = packageSizes;
+            Manufacturer = manufacturer;
+            RemedyFor = remedyFor;
+            ActiveIngredients = activeIngredients;
+            SideEffects = sideEffects;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int[] PackageSizes { get; set; }
+        public string Manufacturer { get; set; }
+        public string[] RemedyFor {  get; set; }
+        public string[] ActiveIngredients { get; set; }
+        public string[] SideEffects { get; set; }
+    }
+}
---
+++ PillPal/PillPalLib/Reminder.cs
@@ -0,0 +1,18 @@
+﻿using PillPalLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillPalLib
+{
+    public class Reminder : IIdentified
+    {
+        public int Id {  get; set; }
+        public TimeOnly When { get; set; }
+        public double DoseCount { get; set; }
+        public int DoseMg {  get; set; }
+        public int TakingMethod { get; set; }
+    }
+}
