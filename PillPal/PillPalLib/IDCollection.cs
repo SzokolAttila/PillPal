49a89a250ba68a4c10ee45bf168e83bb6358f907
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2024-11-04 11:32:14 +0100

    IDCollection (generic collection added)

---
+++ PillPal/PillPalLib/IDCollection.cs
@@ -0,0 +1,28 @@
+﻿using PillPalLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillPalLib
+{
+    public class IDCollection<T> where T : IIdentified
+    {
+        private readonly IEnumerable<T> values;
+        public List<T> Values => values.ToList();
+
+        public IDCollection(IEnumerable<T> values)
+        {
+            this.values = values;
+        }
+
+        public T this[int id]
+        {
+            get
+            {
+                return Values.FirstOrDefault(x => x.Id == id) ?? throw new ArgumentException("Could not find element with this ID");
+            }
+        }
+    }
+}
