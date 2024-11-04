9212c86240aa5f3720be337b5660c810b913975a
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-04 17:14:15 +0100

    created (not implemented) datastore

---
+++ PillPal/PillPalAPI/Model/DataStore.cs
@@ -0,0 +1,72 @@
+﻿using PillPalLib;
+
+namespace PillPalAPI.Model
+{
+    public class DataStore : IDataStore
+    {
+        public Medicine Add(Medicine item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Add(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reminder Add(Reminder item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Medicine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Update(Medicine item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reminder Update(Reminder item)
+        {
+            throw new NotImplementedException();
+        }
+
+        User IItemStore<User>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Reminder IItemStore<Reminder>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<User> IItemStore<User>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Reminder> IItemStore<Reminder>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PillPal/PillPalAPI/Model/IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using PillPalLib;
+
+namespace PillPalAPI.Model
+{
+    public interface IDataStore : IItemStore<Medicine>, IItemStore<User>, IItemStore<Reminder>
+    {
+
+    }
+    public interface IItemStore<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        T Add(T item);
+        T Update(T item);
+        void Delete(int id);
+    }
+}
