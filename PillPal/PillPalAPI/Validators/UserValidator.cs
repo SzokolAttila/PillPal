d8df167b2d34da525a02bd6a7c397b26a9bb005e
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-06 17:30:45 +0100

    updated UserValidator and JWT key

--- PillPal/PillPalAPI/Validators/UserValidator.cs
+++ PillPal/PillPalAPI/Validators/UserValidator.cs
@@ -1,13 +1,16 @@
 ﻿using FluentValidation;
 using PillPalAPI.DTOs.UserDTOs;
+using PillPalAPI.Model;
+using PillPalLib;
 
 namespace PillPalAPI.Validators
 {
     public class UserValidator : AbstractValidator<CreateUserDto>
     {
-        public UserValidator()
+        public UserValidator(IItemStore<User> data)
         {
             RuleFor(x => x.UserName).Length(6, 20).WithMessage("Your username needs to be between 6 and 20 characters.");
+            RuleFor(x => x.UserName).Must(x => !data.GetAll().Any(y => y.UserName == x)).WithMessage("Username already in use.");
             RuleFor(x => x.Password).Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
                 .WithMessage("Your password needs to include at least 8 characters, " +
                 "both upper and lowercase letters, a number, and a special character (@$!%*?&).");
