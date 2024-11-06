31edeb0f9c1ec7456f231580c687acc264be00b7
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-06 17:16:33 +0100

    fixed Validator

--- PillPal/PillPalAPI/Validators/UserValidator.cs
+++ PillPal/PillPalAPI/Validators/UserValidator.cs
@@ -8,7 +8,7 @@ namespace PillPalAPI.Validators
         public UserValidator()
         {
             RuleFor(x => x.UserName).Length(6, 20).WithMessage("Your username needs to be between 6 and 20 characters.");
-            RuleFor(x => x.PasswordText).Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
+            RuleFor(x => x.Password).Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
                 .WithMessage("Your password needs to include at least 8 characters, " +
                 "both upper and lowercase letters, a number, and a special character (@$!%*?&).");
         }
