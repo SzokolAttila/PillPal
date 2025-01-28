cbfc543a121e9110164a7b84e910e5ae2642d6e2
SzokolAttila <szokol.ati2005@gmail.com>
2025-01-28 08:53:09 +0100

    added ContentViews folder and created Light and Dark Themes

---
+++ PillPal/PillPalMAUI/Resources/ContentViews/LogoContentView.xaml.cs
@@ -0,0 +1,9 @@
+namespace PillPalMAUI.Resources.ContentViews;
+
+public partial class LogoContentView : ContentView
+{
+	public LogoContentView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PillPal/PillPalMAUI/Resources/Themes/LightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace PillPalMAUI.Resources.Themes;
+
+public partial class LightTheme : ResourceDictionary
+{
+	public LightTheme()
+	{
+		InitializeComponent();
+	}
+}
