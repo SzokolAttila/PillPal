1396e7f625b92bd578f18a33feca123d1c213aa4
SzokolAttila <szokol.ati2005@gmail.com>
2025-01-28 10:01:06 +0100

    created ContentView for Logo

--- PillPal/PillPalMAUI/MauiProgram.cs
+++ PillPal/PillPalMAUI/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace PillPalMAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("PlayfairDisplay-VariableFont_wght.ttf", "PlayfairDisplay");
                 });
 
 #if DEBUG
