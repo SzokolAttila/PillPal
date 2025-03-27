65f479a6442683ce23f04ee82d7f3b13f3eeee80
SzokolAttila <szokol.ati2005@gmail.com>
2025-03-27 14:03:51 +0000

    added auto-redirect to router

--- PillPalWebApp/src/router/index.js
+++ PillPalWebApp/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
+import { useAdminStore } from '@stores/AdminStore.mjs';
 
 export const router = createRouter({
   history: createWebHistory(),
   linkActiveClass: 'active',
   routes
 })
+
+router.beforeEach((to, from) => {
+  if (to.name != 'login' && useAdminStore().token == null){
+    alert("Ehhez a művelethez be kell jelentkeznie!");
+    return { name: 'login' };
+  }
+});
