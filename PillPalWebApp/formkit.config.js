fed565f98bfe3e0983e15468f223a943456e19b6
SzokolAttila <szokol.ati2005@gmail.com>
2025-04-01 09:13:22 +0000

    added border to input fields for better visibility

--- PillPalWebApp/formkit.config.js
+++ PillPalWebApp/formkit.config.js
@@ -6,7 +6,7 @@ export default defaultConfig({
     config: {
         classes: generateClasses({
             global: {
-                input: 'my-2 form-input rounded-md p-2 w-full',
+                input: 'my-2 form-input rounded-md p-2 w-full border-2 border-component-light dark:border-component-dark',
                 label: 'text-textColor-light dark:text-textColor-dark',
                 message: 'text-errorColor-light dark:text-errorColor-dark font-bold',
             },
