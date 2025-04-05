ebfa8cb05a1a8adf1779da092c9584e0b8a2cd6b
SzokolAttila <szokol.ati2005@gmail.com>
2025-04-05 22:23:35 +0000

    added form for creating, updating and deleting PackageUnits

--- PillPalWebApp/src/stores/PackageUnitStore.mjs
+++ PillPalWebApp/src/stores/PackageUnitStore.mjs
@@ -11,6 +11,20 @@ export const usePackageUnitStore = defineStore('packageUnit-store', {
     async getPackageUnits(){
       let resp = await http.get('PackageUnit');
       this.packageUnits = resp.data;
+    },
+    async postPackageUnit(data){
+      const response = await http.post('PackageUnit', data)
+      this.packageUnits.push(response.data);
+    },
+    async destroyPackageUnit(id){
+      await http.delete(`PackageUnit/${id}`);
+      const idx = this.packageUnits.findIndex(x => x.id == id);
+      this.packageUnits.splice(idx, 1);
+    },
+    async updatePackageUnit(id, data){
+      const response = await http.put(`PackageUnit/${id}`, data);
+      const idx = this.packageUnits.findIndex(x => x.id == id);
+      this.packageUnits.splice(idx, 1, response.data);
     }
   },
   getters: {
