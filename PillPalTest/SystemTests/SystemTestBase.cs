bd88c5282207813487818e39b34c058e91040e3a
SzokolAttila <szokol.ati2005@gmail.com>
2025-04-24 20:23:41 +0200

    tested admin interface's LoginPage via selenium

--- PillPalTest/SystemTests/SystemTestBase.cs
+++ PillPalTest/SystemTests/SystemTestBase.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using PillPalLib.APIHandlers;
 using PillPalLib.DTOs.UserDTOs;
 using System;
@@ -11,8 +12,9 @@ namespace PillPalTest.SystemTests
 {
     public class SystemTestBase
     {
-        protected WebDriver driver;
-        protected LoginDto admin;
+        protected WebDriverWait? wait;
+        protected WebDriver? driver;
+        protected LoginDto? admin;
         protected readonly CreateUserDto adminLogin = new () { UserName = "administrator", Password = "aA1?aA1?" };
         protected const string url = "http://vm1.test:5173/";
         protected readonly UserAPIHandler handler = new();
